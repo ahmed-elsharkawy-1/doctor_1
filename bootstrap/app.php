0995491243fc6cd08ec070806af998251c205958
ahmed-elsharkawy-1 <ahmedme3394@gmail.com>
2026-08-08 20:56:37 +0300

    Trust reverse proxy for production assets

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,6 +26,8 @@ return Application::configure(basePath: dirname(__DIR__))
         apiPrefix: 'api/v1',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->api(prepend: [
             SetApiLocale::class,
         ]);
